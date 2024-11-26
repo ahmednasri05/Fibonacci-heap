d15b7bd640a0b4a749e3c6183aeeed9be3455b8e
ahmednasri05 <23P0270@eng.asu.edu.eg>
2024-11-26 18:13:50 +0200

    implemented extractmin(), consolidate(), swap() , added method structure

--- Fibonacci_Heap.h
+++ Fibonacci_Heap.h
@@ -4,11 +4,15 @@
 #include <iostream>
 #include <unordered_map>
 #include <cmath>
+// Conventions: NIL = nullptr
+//              Node* left and right pointers are used to form a circular doubly linked list
+//              Node* child pointer is used to point to the leftmost child
 
 template <typename ValueType>
 class FibonacciHeap {
 public:
     struct Node {
+        public:  // Node must tbe private , because the user should not be able to create a node without the heap
         ValueType value;
         int degree;
         bool marked;
@@ -24,28 +28,28 @@ public:
     FibonacciHeap();
     ~FibonacciHeap();
 
-    void insert(ValueType value);
-    void merge(FibonacciHeap& other);
-    bool isEmpty() const;
-    Node* extractMin() const;
-    void deleteMin();
-    void decreaseKey(Node* node, ValueType newValue);
-    void deleteNode(Node* node);
-    Node* getMin() const;
+    void insert(ValueType value); // mohamed tarek
+    void merge(FibonacciHeap& other); // mohamed tarek
+    bool isEmpty() const; // john, farrag
+    Node* extractMin(); // nasri, farrag
+    void decreaseKey(Node* node, ValueType newValue); // hady
+    void deleteNode(Node* node); // hady
+    Node* getMin() const; //john
 
 private:
     Node* minNode;
     int nodeCount;
 
     //Used by decrease key to clean up the heap to be in the fib sequence
-    void consolidate();
+    void consolidate(); // nasri
     //Used by the consolidate function to make a node in the root list become a child
-    void link(Node* y, Node* x);
+    void link(Node* y, Node* x); // mohamed tarek
     //Used by decrease key to remove a child and append it to the root list
-    void cut(Node* x, Node* y);
+    void cut(Node* x, Node* y); //hady NOTE: check if this is the last child of y, if so, set y->child to nullptr
     //Used by decrease key to propagate the cut upwards through the tree, recursively until a non-marked node is found or the root is reached.
-    void cascadingCut(Node* y);
-    void deleteAll(Node* node);
+    void cascadingCut(Node* y); // hady
+    void deleteAll(Node* node); // farrag
+    void swap(Node*& x, Node*& y);
 };
 
 #endif // FIBONACCI_HEAP_H
